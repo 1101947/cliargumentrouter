ffff6340602e902880f970732c276d3f5d988f25
hsfhrwe4r <hsfhrwe4r>
2026-04-24 17:54:59 +0300

    moved almost everything,except flag and cmdrouter to old, leaft only Handler and Process function

--- cmdrouter/cmdrouter.go
+++ cmdrouter/cmdrouter.go
@@ -17,8 +17,25 @@ func (R ProcesserFunc) Process(posargs []string) error {
 	return nil
 }
 
-type Router interface {
-	Handle(path string, handler Handler) 
-	HandleFunc(path string, fn func(posargs []string) error)
-	Process(posargs []string) error
-}
+//type DRouter map[string]Handler 
+//
+//
+//func (D DRouter) Handle(posargs []string, H Handler) error {
+//}
+//
+//func (D DRouter) HandleFunc(path []string, PF ProcesserFunc) error {
+//}
+//
+//func (D DRouter) Process(posargs []string) error {
+//	flags := flag.DefaultFlags("--", "=", posargs)
+//	err := flags.Parse()
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	_, posargs := flags.Extract() // TODO: do something with kwargs, mb process help
+//	path := strings.Join(posargs, " ")
+//	handler := d.findHandler(path)
+//	handler.Run(posargs)
+//}
+
+
